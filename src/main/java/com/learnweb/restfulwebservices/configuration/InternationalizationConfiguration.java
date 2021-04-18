f5d37b3c76f85195407fa7b1950d9cf27ef85daf
SyedAanif <66770875+SyedAanif@users.noreply.github.com>
2021-04-18 15:56:37 +0530

    Add files via upload

---
+++ src/main/java/com/learnweb/restfulwebservices/configuration/InternationalizationConfiguration.java
@@ -0,0 +1,27 @@
+package com.learnweb.restfulwebservices.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class InternationalizationConfiguration {
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource bundleMessageSource(){
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("messages");
+        return resourceBundleMessageSource;
+    }
+}
