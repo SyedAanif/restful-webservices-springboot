d18745647213906e2711aade7e9d824fd32a811a
SyedAanif <66770875+SyedAanif@users.noreply.github.com>
2021-04-18 15:48:16 +0530

    Create RestfulWebservicesApplicationTests.java

---
+++ src/test/java/com/learnweb/restfulwebservices/RestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnweb.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
